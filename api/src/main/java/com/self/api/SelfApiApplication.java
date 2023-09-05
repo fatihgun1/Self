ea5085ef7053f292c5d2462367b8cd2a2c875cce
Fatih Gün <fatihgun17.35@gmail.com>
2023-09-05 21:52:27 +0300

    first commit

---
+++ api/src/main/java/com/self/api/SelfApiApplication.java
@@ -0,0 +1,13 @@
+package com.self.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfApiApplication.class, args);
+	}
+
+}
